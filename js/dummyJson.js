259d0be1c122a192edd3b9b286710e6773704683
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-24 09:31:37 +0300

    get posts from dummy

--- js/dummyJson.js
+++ js/dummyJson.js
@@ -3,3 +3,14 @@ console.log('dummyJson.js file was loaded');
 
 // pariusti ir isknosolinti tik postus
 // https://dummyjson.com/posts
+
+const urlPosts = 'https://dummyjson.com/posts?limit=10';
+// https://dummyjson.com/posts?limit=10&skip=10&select=title,reactions,userId
+fetch(urlPosts)
+  .then((resp) => resp.json())
+  .then((dataInJs) => {
+    console.log('dataInJs ===', dataInJs);
+    const postsArr = dataInJs.posts;
+    console.log('postsArr ===', postsArr);
+  })
+  .catch((error) => console.warn(error));
