f456b5b3257ee2ac40d982be8341212691537484
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-24 10:05:46 +0300

    single post

---
+++ js/single-post.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log('single-post.js file was loaded');
+
+// paimti url parametro pavadinimu postId reiksme
+
+// ir ja isspausdinti
+
+// parsiusti konkretu posta paga id
+
+//'https://dummyjson.com/posts/1'
